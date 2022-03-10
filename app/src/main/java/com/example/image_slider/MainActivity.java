36ffeb1df4ed7515612656f18777d163e5f9bd5c
Bhatt Nisarg <nmbhatt2001@gmail.com>
2022-03-10 10:54:43 +0530

    add second activity viewflipper

--- app/src/main/java/com/example/image_slider/MainActivity.java
+++ app/src/main/java/com/example/image_slider/MainActivity.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     //creating object of viewpager
     ViewPager viewPager;
 
+    int currentPage = 0;
+    Timer timer;
+    final long DELAY_MS = 1000; //delay in milisecond before task is executed
+    final long PERIOD_MS = 3000; // time in milliseconds between successive task execution
     int[] images = {R.drawable.doramon,R.drawable.jerry,R.drawable.micky,R.drawable.tiger};
 
     //creating object of viewpagerAdapter
@@ -27,5 +35,27 @@ public class MainActivity extends AppCompatActivity {
 
         //Adding the Adapter to the viewPager
         viewPager.setAdapter(viewPagerAdapter);
+
+
+        final Handler handler = new Handler();
+        final Runnable update = new Runnable() {
+            @Override
+            public void run() {
+                if (currentPage == images.length - 1){
+                    currentPage=0;
+                }
+                viewPager.setCurrentItem(currentPage++,true);
+            }
+        };
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+
+        },DELAY_MS,PERIOD_MS);
+
+
     }
 }
